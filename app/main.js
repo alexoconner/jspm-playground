6ab97e6af87c3b056f8e5bb4c787824ee8c62a07
dotlabel-alex <alexander.skrabl@dotlabel.co.uk>
2015-05-12 14:41:14 +0100

    initial commit, jspm with react

---
+++ app/main.js
@@ -0,0 +1,21 @@
+
+import React from 'react';
+
+class App extends React.Component {
+    // constructor(props) {
+    //     super(props);
+    //
+    //     console.log('hello react world');
+    // }
+
+    render() {
+        return (
+            <div>
+                test
+            </div>
+        );
+    }
+}
+
+
+React.render(<App />, document.body);

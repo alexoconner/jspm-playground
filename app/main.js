28d333eac2b8bc174763aaecf17c1845a5b38ac0
= <=>
2015-05-18 00:24:51 +0100

    simple nav and view component

--- app/main.js
+++ app/main.js
@@ -1,22 +1,54 @@
 
 import React from 'react';
-import Navigation from './modules/navigation';
+import NavLink from './modules/navLink';
 import View from './modules/views';
 
 class App extends React.Component {
     constructor(props) {
-         super(props);
+        super(props);
 
-         console.log('playground react app');
+        console.log('playground react app');
+
+        this.state = {
+            view: 'home'
+        };
+
+        this.onNavClick = this.onNavClick.bind(this);
+    }
+
+    //getInitialState() {
+    //    return {
+    //        view: 'home'
+    //    };
+    //}
+
+    onNavClick(view) {
+        console.log('clicked nav link: ' + view);
+        this.setState({
+            view: view
+        });
     }
 
     render() {
         return (
             <div>
                 <h1>React JS App with Jspm Package Management</h1>
-                <Navigation />
+                <nav>
+                    <ul>
+                        <NavLink
+                            title="Home"
+                            view="home"
+                            onClick={ this.onNavClick }
+                        />
+                        <NavLink
+                            title="About"
+                            view="about"
+                            onClick={ this.onNavClick }
+                        />
+                    </ul>
+                </nav>
                 <div className="container">
-                    <View />
+                    <View view={ this.state.view } />
                 </div>
             </div>
         );
